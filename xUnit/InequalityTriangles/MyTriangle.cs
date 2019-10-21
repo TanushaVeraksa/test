5247886ab2c75b871a8e6f3d303b96e121347c50
Veraksa Tansyana <veraksa161@gmail.com>
2019-10-21 16:57:34 +0300

    Added xUnit

---
+++ xUnit/InequalityTriangles/MyTriangle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InequalityTriangles
+{
+    public class MyTriangle
+    {
+        public static bool TheExistenceOfTriangle(double X, double Y, double Z)
+        {
+            return (X > 0 && Y > 0 && Z > 0 && X <= Y + Z && Y <= X + Z && Z <= X + Y);
+        }
+    }
+}
