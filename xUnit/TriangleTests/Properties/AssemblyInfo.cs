7769dc5bcab423a4d0a3ffa89076f7950b878b82
Veraksa Tansyana <veraksa161@gmail.com>
2019-10-22 13:09:42 +0300

    Added xUnit

---
+++ xUnit/TriangleTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f31780b8-bf10-4d53-983b-509e61877bec")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ xUnit/TriangleTests/TriangleTests.cs
@@ -0,0 +1,64 @@
+﻿using InequalityTriangles;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InequalityTriangles.Test
+{
+    [TestClass]
+    public class TriangleTests
+    {
+        [TestMethod]
+        public void IsTrianglePossibleEquilateralTriangle()
+        {
+            Assert.IsTrue(Triangle.TheExistenceOfTriangle(15, 15, 15));
+        }
+
+        [TestMethod]
+        public void IsTrianglePossibleEgyptianTriangle()
+        {
+            Assert.IsTrue(Triangle.TheExistenceOfTriangle(3, 4, 5));
+        }
+
+        [TestMethod]
+        public void IsTrianglePossibleSumLessThanSideLength()
+        {
+            Assert.IsFalse(Triangle.TheExistenceOfTriangle(10, 20, 35));
+        }
+
+        [TestMethod]
+        public void IsTrianglePossibleZeroLengthSide()
+        {
+            Assert.IsFalse(Triangle.TheExistenceOfTriangle(0, 10, 20));
+        }
+
+        [TestMethod]
+        public void IsTrianglePossibleTwoZeroLengthSide()
+        {
+            Assert.IsFalse(Triangle.TheExistenceOfTriangle(0, 0, 10));
+        }
+        [TestMethod]
+        public void IsTrianglePossibleThreeZeroLengthSide()
+        {
+            Assert.IsFalse(Triangle.TheExistenceOfTriangle(0, 0, 0));
+        }
+        [TestMethod]
+        public void IsTrianglePossibleOneNegativeLengthSide()
+        {
+            Assert.IsFalse(Triangle.TheExistenceOfTriangle(-10, 10, 10));
+        }
+        [TestMethod]
+        public void IsTrianglePossibleTwoNegativeLengthSide()
+        {
+            Assert.IsFalse(Triangle.TheExistenceOfTriangle(-10, 10, -10));
+        }
+        [TestMethod]
+        public void IsTrianglePossibleAllNegativeLengthSide()
+        {
+            Assert.IsFalse(Triangle.TheExistenceOfTriangle(-10, -10, -10));
+        }
+        [TestMethod]
+        public void IsTrianglePossibleSumMoreThanSideLength()
+        {
+            Assert.IsTrue(Triangle.TheExistenceOfTriangle(15, 25, 30));
+        }
+    }
+}
